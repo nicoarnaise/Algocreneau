4df918cf4d800e888a29db5208bc21d794b67774
Unknown <nicolas.arnaise@telecom-st-etienne.fr>
2017-04-27 02:07:52 -0400

    import OpenCV

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import java.lang.reflect.Field;
+
 import org.opencv.core.Core;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		//chargement de la lib OpenCV
-		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+		try {
 
+	        System.setProperty("java.library.path", "lib/");
+
+	        Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+	        fieldSysPath.setAccessible(true);
+	        fieldSysPath.set(null, null);
+
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	        throw new RuntimeException(ex);
+	    }
+		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+		
 		Menu me = new Menu();
 		me.setVisible(true);
 	}	
